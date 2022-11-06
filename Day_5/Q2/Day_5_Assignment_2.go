739205c11b32ce28c3b95f4248fc21a599cf3e02
unknown <SantosaArymurti@bitbucket.org>
2022-11-07 04:09:40 +0700

    Assignment Q2 Week 1

--- Day_5/Q2/Day_5_Assignment_2.go
+++ Day_5/Q2/Day_5_Assignment_2.go
@@ -34,10 +34,18 @@ type Cache struct {
 	IsFilled   bool
 }
 
-func (c *Cache) aggregate() {
+// Deklarasi variabel untuk cache kosong
+var zeroCache = &Cache{}
+
+func (c *Cache) aggregate(i int) {
 	wg := sync.WaitGroup{}
 
+	//Number of goroutines for getDescription() and getJobList()
 	wg.Add(2)
+
+	//Waktu awal memulai
+	start := time.Now()
+	fmt.Println("dari calculate ", start)
 
 	if !c.IsFilled {
 
@@ -55,23 +63,55 @@ func (c *Cache) aggregate() {
 		c.IsFilled = true
 	}
 
-	fmt.Println(c.DescString)
-	fmt.Println(c.JobList)
+	//Print Description and JobList from API, we only need the runtime of the program, so we disable it
+	//fmt.Println(c.DescString)
+	//fmt.Println(c.JobList)
+
+	//Waktu sejak operasi aggregate dimulai
+	fmt.Printf("took %v\n", time.Since(start))
+
+	//Saat aggregate dipanggil genap akan mengosongkan cache dengan zeroCache -> declared diatas
+	if i%2 == 0 {
+		*c = *zeroCache
+
+		//Deklarasikan boolean  isFilled jadi false lagi for safe measure
+		c.IsFilled = false
+	}
 }
 
 func main() {
-	start1 := time.Now()
-	fmt.Println("dari calculate ", start1)
 
-	chc := Cache{}
-	chc.aggregate()
+	//Declare cache
+	var c Cache
 
-	fmt.Printf("took %v\n", time.Since(start1))
+	//Insert number of loops -> berapa kali aggregate() dipanggil
+	fmt.Print("Masukkan jumlah looping: ")
+	var n int
+	fmt.Scanf("%d", &n)
 
-	start2 := time.Now()
-	fmt.Println("dari calculate ", start2)
+	//For loops of calling method aggregate() n times
+	for i := 1; i <= n; i++ {
+		c.aggregate(i)
+	}
 
-	chc.aggregate()
+	//Disaat ganjil nilai runtime akan lebih lama dibandingkan saat looping genap, karena pada looping genap sudah memiliki cache
+	//Setelah looping genap selesai cache akan dikosongkan sehingga Cache c perlu diisi kembali
 
-	fmt.Printf("took %v\n", time.Since(start2))
+	//Contoh output
+	// Masukkan jumlah looping: 6
+	// dari calculate  2022-11-07 04:05:33.4636118 +0700 +07 m=+0.468313201
+	// took 341.3058ms
+	// dari calculate  2022-11-07 04:05:33.8049176 +0700 +07 m=+0.809619001
+	// took 529.6µs
+	// dari calculate  2022-11-07 04:05:33.8054472 +0700 +07 m=+0.810148601
+	// took 257.0132ms
+	// dari calculate  2022-11-07 04:05:34.0624604 +0700 +07 m=+1.067161801
+	// took 561µs
+	// dari calculate  2022-11-07 04:05:34.0630214 +0700 +07 m=+1.067722801
+	// took 258.0837ms
+	// dari calculate  2022-11-07 04:05:34.3216144 +0700 +07 m=+1.326315801
+	// took 73.1µs
+
+	//Berkat goroutine, fungsi aggregate tanpa cache sudah termasuk cepat (dalam milisecond)
+	//Tetapi bisa lebih cepat bila local system memiliki memori untuk menyimpan value yang dipanggil berulang-ulang (mencapai microsecond)
 }
